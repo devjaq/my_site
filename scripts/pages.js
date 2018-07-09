03936f6f61221bb1ba8762a80b9114a380c1bcee
devjaq <devjaq@gmail.com>
2018-07-09 12:07:40 -0400

    style cleanup

--- scripts/pages.js
+++ scripts/pages.js
@@ -5,11 +5,11 @@ const home = {
   <main>
     <section class="content flex-sm-column fifties-border">
       <h3>Hi, thanks for being here!</h3>
-      <p>I am currently working hard in a Grand Circus coding bootcamp, but don't worry! I will be in the job market very soon.</p>
-      <p>If you are here to learn a few things <a href="#!/about">about me</a>, why don't you check out my <a href="Resume.pdf">résumé</a> and <a href="https://github.com/devjaq">portfolio</a>?</p>
+      <p>I am a new front-end web developer who just graduated from a Grand Circus coding bootcamp. I'm currenly looking for a full-time position and projects to work on!</p>
+      <p>If you are here to learn a few things <!--<a href="#!/about">-->about me<!--</a>-->, why don't you check out my <a href="Resume.pdf" target="_blank">résumé</a> and <a href="https://github.com/devjaq">portfolio</a>?</p>
     </section>
     <section class="content flex-sm-column fifties-border">
-      <p>Currently I'm studying front-end development at Grand Circus Detroit.</p>
+      <p>Currently I'm a graduate of the front-end bootcamp at Grand Circus Detroit.</p>
       <p> I have a unique background which helped foster a particular set of skills. Attention to detail came from a patent law firm, teamwork from professionally cooking, and my ability to work under pressure came from both. </p>
       <p>I’m looking for opportunities as an entry-level developer where I will be encouraged to learn from and work with more experienced mentors.</p>
   </section>
@@ -34,11 +34,15 @@ const home = {
 
 const resume = {
   template: `
-
+    
     <object
     class="resume" 
     data="Resume.pdf"
     ></object>
+
+    <!--
+    <img src="Resume.pdf"></img>
+    -->
 
   `
 }
