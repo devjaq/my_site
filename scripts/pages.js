e0a82af8822bb5f77b2d11f13c6f05a3c05cb15b
devjaq <devjaq@gmail.com>
2018-08-31 00:34:15 -0400

    style cleanup. edit paragraphs on home page.

--- scripts/pages.js
+++ scripts/pages.js
@@ -3,14 +3,14 @@
 const home = {
   template: `
   <main>
-    <section class="content flex-sm-column fifties-border">
+    <section class="content">
       <h3>Hi, thanks for being here!</h3>
-      <p>I am a new front-end web developer who just graduated from a Grand Circus coding bootcamp. I'm currenly looking for a full-time position and projects to work on!</p>
-      <p>If you are here to learn a few things <!--<a href="#!/about">-->about me<!--</a>-->, why don't you check out my <a href="Resume.pdf" target="_blank">résumé</a> and <a href="https://github.com/devjaq">portfolio</a>?</p>
+      <p>I'm a junior web developer specializing in JavaScript, HTML, CSS, and PHP. </p>
+      <p>I learned JavaScript from the Grand Circus Detroit coding bootcamp and I'm currently learning PHP and WordPress on my own. I'm currenly looking for a full-time position and projects to work on!</p>
+      <p>If you are here to learn a few things <!--<a href="#!/about">-->about me<!--</a>-->, why don't you check out my <a href="Jacquelyn_Resume.pdf" target="_blank">résumé</a> and <a href="https://github.com/devjaq">portfolio</a>?</p>
     </section>
-    <section class="content flex-sm-column fifties-border">
-      <p>Currently I'm a graduate of the front-end bootcamp at Grand Circus Detroit.</p>
-      <p> I have a unique background which helped foster a particular set of skills. Attention to detail came from a patent law firm, teamwork from professionally cooking, and my ability to work under pressure came from both. </p>
+    <section class="content">
+      <p>My non-technical background tells you that I have strong teamwork and communication skills as well as a drive to provide an elegant experience for users. Feel free to ask me why sometime! (Spoiler: both of my previous fields are plagued with bad software)</p>
       <p>I’m looking for opportunities as an entry-level developer where I will be encouraged to learn from and work with more experienced mentors.</p>
   </section>
   </main>
