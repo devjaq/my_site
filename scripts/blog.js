0c5ec260badff728d5713dab1f900ceda420acd7
devjaq <devjaq@gmail.com>
2018-10-26 21:28:49 -0400

    blog header is sticky now and added tagline

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,8 +3,21 @@
 const blogSections = document.getElementsByClassName('blog-sections');
 const curate = document.getElementsByClassName('curate');
 const design = document.getElementsByClassName('design');
+const header = document.getElementsByTagName('header')[0];
 
-console.log(blogSections[0]);
+// console.log(blogSections[0]);
+console.log(header);
 
 
+window.onscroll = function() {
+  
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){  
+    console.log("scroll");
+    
+    header.classList.add("sticky");
+}
+else{
+    header.classList.remove("sticky");
+}
+}
 
