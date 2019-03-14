f00d9b2243625669e4b9bc501021ad56d9350579
Jacquelyn Olson <jaq@Jacquelyns-Mac-mini.local>
2019-03-13 22:33:15 -0400

    finished header redesign. major progress on Sass conversion

--- app/scripts/components/logo.js
+++ app/scripts/components/logo.js
@@ -3,12 +3,13 @@
 const logo = {
   template: `
   <section class="logo">
-    <h1 class="dev-jaq">devJaq</h1>
+    <div class="devjaq-square">
+      <h1 class="devjaq">devJaq</h1>
+      <div class="bg-circle"></div>
+    </div>
     <h2 class="myname"> by Jaq Aimee</h2>
   </section>
   `
-}
+};
 
-angular
-  .module("Site")
-  .component("logo", logo)
+angular.module("Site").component("logo", logo);
