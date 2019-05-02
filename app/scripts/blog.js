a960dec52b85e21d401e00e464e1fc1437255aeb
devjaq <devjaq@gmail.com>
2019-05-02 00:44:57 -0400

    improved sticky header on blog

--- app/scripts/blog.js
+++ app/scripts/blog.js
@@ -6,19 +6,32 @@ const design = document.getElementsByClassName('design');
 const header = document.getElementsByTagName('header')[0];
 
 // console.log(blogSections[0]);
+// console.log(header);
+
+// let header = document.getElementsByTagName('header');
 console.log(header);
 
 
 window.onscroll = function () {
+  console.log(header.scrollTop, document.documentElement.scrollTop);
 
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+
+  if (document.documentElement.scrollTop > 50 && document.documentElement.scrollTop < 100 || document.documentElement.scrollTop > 200) {
     console.log("scroll");
 
     header.classList.add("sticky");
+    if (document.documentElement.scrollTop < 100) {
+      document.documentElement.scrollTop = 100;
+    }
+    console.log("sticky");
+    // setTimeout(function(){ console.log("timer!"); }, 3000);
   }
-  else {
-    header.classList.remove("sticky");
+  else if (document.documentElement.scrollTop == 0) {
+    setTimeout(function(){ header.classList.remove("sticky"); }, 500);
+    // header.classList.remove("sticky");
+    console.log("not sticky");
+
   }
-}
+};
 
 // when you mouse over "curate" or "design" the matching text appears in an overlay
